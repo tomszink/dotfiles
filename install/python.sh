f56db206d23846c71dfee1ab237efdb2cc4e5dd7
Toms Zink <toms.zink@gmail.com>
2020-02-02 19:18:24 +0100

    Add python installation script with pipx

---
+++ install/python.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Python related installations like pipx
+set -o errexit
+set -o pipefail
+set -o nounset
+set -x
+
+pipx_packages=(
+  black
+  flake8
+  pipenv
+  tox
+)
+
+for pipx_package in "${pipx_packages[@]}"; do
+  if [[ $(command -v $pipx_package) ]]
+  then
+    pipx upgrade "$pipx_package"
+  else
+    pipx install "$pipx_package"
+  fi
+done
